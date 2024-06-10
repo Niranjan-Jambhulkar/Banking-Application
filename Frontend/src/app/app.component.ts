a4ba98d331e0446a62162d74684a4a75f7911f8d
Niranjan <niranjan.jambhulkar@outlook.com>
2024-06-10 21:42:52 +0530

    Initial commit

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, CommonModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'Frontend';
+  title = 'Word Bank';
+
 }
