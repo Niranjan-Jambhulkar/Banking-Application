f81d5259c6027d748a76ec3a121f2ec2395a4f63
Niranjan <niranjan.jambhulkar@outlook.com>
2024-06-11 20:39:31 +0530

    initial commit

--- Frontend/src/app/view-balance/view-balance.component.ts
+++ Frontend/src/app/view-balance/view-balance.component.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-view-balance',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './view-balance.component.html',
   styleUrl: './view-balance.component.css'
 })
@@ -11,4 +15,13 @@ export class ViewBalanceComponent {
   accNo:any = 124568964789;
   ifsc:any = "HDGF00145";
   balance:any = 500000;
+  data:any;
+
+  constructor(private http: HttpClient){
+
+  }
+
+  get(){
+    this.http.get("http://localhost:8080/acco").subscribe((data)=>{this.data = data});
+  }
 }
