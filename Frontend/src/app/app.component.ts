a525f569292b12d5aea46578c08dcc477dddded1
Niranjan <niranjan.jambhulkar@outlook.com>
2024-06-11 13:55:09 +0530

    Initial commit

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
-
-
-
+import { ViewBalanceComponent } from './view-balance/view-balance.component';
+import { DepositComponent } from './deposit/deposit.component';
+import { WithdrawComponent } from './withdraw/withdraw.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, CommonModule, RouterModule],
+  imports: [RouterOutlet, RouterLink, CommonModule, RouterModule, WithdrawComponent, ViewBalanceComponent, DepositComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ViewBalanceComponent } from './view-balance/view-balance.component';
+import { DepositComponent } from './deposit/deposit.component';
+import { WithdrawComponent } from './withdraw/withdraw.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "view", component:ViewBalanceComponent},
+    {path: "deposit", component:DepositComponent},
+    {path: "withdraw", component:WithdrawComponent}
+];
