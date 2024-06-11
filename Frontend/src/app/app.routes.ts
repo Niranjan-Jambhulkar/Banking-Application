6164359213ba68460788bf732f617762c66627ee
Niranjan <niranjan.jambhulkar@outlook.com>
2024-06-11 14:05:23 +0530

    Initial commit

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { ViewBalanceComponent } from './view-balance/view-balance.component';
 import { DepositComponent } from './deposit/deposit.component';
 import { WithdrawComponent } from './withdraw/withdraw.component';
+import { TransferComponent } from './transfer/transfer.component';
+import { TransactionComponent } from './transaction/transaction.component';
 
 export const routes: Routes = [
     {path: "view", component:ViewBalanceComponent},
     {path: "deposit", component:DepositComponent},
-    {path: "withdraw", component:WithdrawComponent}
+    {path: "withdraw", component:WithdrawComponent},
+    {path: "transfer", component:TransferComponent},
+    {path: "transaction", component:TransactionComponent}
 ];
